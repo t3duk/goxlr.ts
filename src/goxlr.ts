594a9a4c6e29f48f6ad045c799d1523613a0f183
Teddy Brine <ted@daad.wtf>
2023-12-15 21:58:56 +0000

    chore: 📝 Add goals

---
+++ src/goxlr.ts
@@ -0,0 +1,16 @@
+/*
+
+Main File
+
+In this file we need the following systems:
+
+- Create a GoXLR instance
+- Create a WebSocket connection with the utility with the GoXLR instance
+- Able to close this connection and re-open it
+- Import the different classes and functions from the other files
+  
+*/
+
+export function init(bind: string, port: number, serial: string) {
+  
+}
